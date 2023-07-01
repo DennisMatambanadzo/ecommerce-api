bb383e835d7b1778140f3be3fe82af79f65c0f7b
matambanadzotanakad <49873792+matazdt@users.noreply.github.com>
2023-07-01 13:50:44 +0200

    Stage4 Objectives: 1. Authenticate requests using the JWT token from the login process. 2. Test that the user is being authenticated.

--- src/main/java/com/epoch/ecommercebackend2/model/LocalUser.java
+++ src/main/java/com/epoch/ecommercebackend2/model/LocalUser.java
@@ -1,5 +1,6 @@
 package com.epoch.ecommercebackend2.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 
@@ -19,9 +20,9 @@ public class LocalUser {
     @Column(name = "username", nullable = false, unique = true)
     private String username;
 
+    @JsonIgnore
     @Column(name = "password", nullable = false, length = 1000)
     private String password;
-
 
     @Column(name = "email", nullable = false, unique = true, length = 320)
     private String email;
@@ -32,6 +33,7 @@ public class LocalUser {
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<Address> addresses = new ArrayList<>();
 
